5c01ec723051d5420394c035d8eea75ed28f5f65
3achy <hwp3achy@gmail.com>
2016-01-30 12:54:07 +1030

    adjustments to main script

--- main.R
+++ main.R
@@ -3,8 +3,8 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 #imports all data
-gunrate <- read.csv("../data/gunrate.csv")
-homicides <- read.csv("../data/homicides.csv")
+gunrate <- read.csv("data/gunrate.csv")
+homicides <- read.csv("data/homicides.csv")
 
 #merges data based on country
 data <- merge(gunrate,homicides,by=c("country"))
@@ -12,5 +12,5 @@ data <- merge(gunrate,homicides,by=c("country"))
 
 
 #testing output
-sink("../output/testoutput.txt", append=FALSE, split=TRUE)
+sink("output/testoutput.txt", append=FALSE, split=TRUE)
 cor.test(data$ownership.rate,data$homicide.rate)
