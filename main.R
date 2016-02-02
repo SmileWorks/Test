8ebe55b2d40131531d28ca5975f9dfa1fd6ef06f
SmileWorks <movesthatfunction@outlook.com>
2016-02-02 22:36:22 +1030

    added history files

--- main.R
+++ main.R
@@ -9,8 +9,11 @@ homicides <- read.csv("data/homicides.csv")
 #merges data based on country
 data <- merge(gunrate,homicides,by=c("country"))
 
-
+plot(data$ownership.rate, data$homicide.rate, ann=F)
+title(main="Intentional Homicide Rate Vs Gun Ownership Rate by Country", 
+      xlab="Gun Ownership Rate (per 100 people)",ylab="Intentional Homicide Rate (per 100 000 people)")
+abline(lm(data$homicide.rate~data$ownership.rate),col="red")
 
 #testing output
-sink("output/testoutput.txt", append=FALSE, split=TRUE)
-cor.test(data$ownership.rate,data$homicide.rate)
+#sink("output/testoutput.txt", append=FALSE, split=TRUE)
+#cor.test(data$ownership.rate,data$homicide.rate)
