5548b375749d322389fd4341dad7d95f7d7d548e
3achy <hwp3achy@gmail.com>
2016-02-04 15:39:04 +1030

    added function to code

--- R/gun ownership vs homicide rate.R
+++
@@ -1,5 +0,0 @@
-guns <- read.csv("C:/Users/Nick/Desktop/Research Project/DATA/guns.csv")
-homicidescountry <- read.csv("C:/Users/Nick/Desktop/Research Project/DATA/homicidescountry.csv")
-plot(guns$rate,homicidescountry$rate,main="Intentional Homicide vs Gun Ownership rate by country", xlab="Gun Ownership rate (per 100 people)", ylab="Intentional homicide (per 100 000 people)")
-cor.test(guns$rate,homicidescountry$rate)
-sink("../output/testoutput.txt", append=FALSE, split=FALSE)
--- main.R
+++ main.R
@@ -3,16 +3,42 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 #imports all data
-gunrate <- read.csv("data/gunrate.csv")
-homicides <- read.csv("data/homicides.csv")
+gunOwnershipRate <- read.csv("data/gunOwnershipRate.csv")
+homicideRate <- read.csv("data/homicideRate.csv")
+grossNationalIncomePerCapita <- read.csv("data/grossNationalIncomePerCapita.csv")
 
 #merges data based on country
-data <- merge(gunrate,homicides,by=c("country"))
+data <- merge(GunOwnershipRate,HomicideRate,by=c("country"))
+data <- merge(data, grossNationalIncomePerCapita, by=c("country"))
 
-plot(data$ownership.rate, data$homicide.rate, ann=F)
-title(main="Intentional Homicide Rate Vs Gun Ownership Rate by Country", 
-      xlab="Gun Ownership Rate (per 100 people)",ylab="Intentional Homicide Rate (per 100 000 people)")
-abline(lm(data$homicide.rate~data$ownership.rate),col="red")
+
+#write overall data to file
+write.csv(data, "output/data.csv", row.names=FALSE)
+
+testAndPlotCorrelation <- function(x, y, title, xlabel, ylabel)
+{
+  png(filename=paste("figs/",title,".png"), 
+      units="in", width=10, height=8, pointsize=12, res=72)
+  plot(data$gunOwnershipRate, data$homicideRate, ann=F)
+  title(main=title, 
+        xlab=xlabel,ylab=ylabel)
+  abline(lm(y~x),col="red")
+  dev.off()
+  
+  sink(paste("output/", title,".txt"))
+  print(cor.test(x, y))
+  sink()
+}
+
+testAndPlotCorrelation(data$gunOwnershipRate, data$homicideRate, 
+                       "Intentional Homicide Rate vs Gun Ownership Rate by Country", 
+                "Gun Ownership Rate (per 100 people)", "Intentional Homicide Rate (per 100 000 people)")
+
+testAndPlotCorrelation(data$grossNationalIncomePerCapita, data$homicideRate, 
+                       "Gross National Income Per Capita vs Intentional Homicide Rate by Country", 
+                       "Gross National Income Per Capita (in US Dollars?)", "Intentional Homicide Rate (per 100 000 people)")
+
+
 
 #testing output
 #sink("output/testoutput.txt", append=FALSE, split=TRUE)
