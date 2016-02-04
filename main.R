aae935eaf240867fa4a958398e7d02846def11b1
SmileWorks <movesthatfunction@outlook.com>
2016-02-04 18:05:28 +1030

    preponderance of the data

--- main.R
+++ main.R
@@ -6,11 +6,28 @@ setwd(this.dir)
 gunOwnershipRate <- read.csv("data/gunOwnershipRate.csv")
 homicideRate <- read.csv("data/homicideRate.csv")
 grossNationalIncomePerCapita <- read.csv("data/grossNationalIncomePerCapita.csv")
+HDIRank <- read.csv("data/HDIRank.csv")
+HDIValue <- read.csv("data/HDIValue.csv")
+homicideByFirearmRate <- read.csv("data/homicideByFirearmRate.csv")
+lifeExpectancyAtBirth<- read.csv("data/lifeExpectancyAtBirth.csv")
+meanYearsOfSchooling <- read.csv("data/meanYearsOfSchooling.csv")
+region <- read.csv("data/region.csv")
+subregion <- read.csv("data/subregion.csv")
+#averageIQ <- read.csv("data/averageIQ.csv")
+#violentCrime <- read.csv("data/violentCrime.csv")
 
 #merges data based on country
 data <- merge(GunOwnershipRate,HomicideRate,by=c("country"))
 data <- merge(data, grossNationalIncomePerCapita, by=c("country"))
-
+data <- merge(data, HDIRank, by=c("country"))
+data <- merge(data, HDIValue, by=c("country"))
+data <- merge(data, homicideByFirearmRate, by=c("country"))
+data <- merge(data, lifeExpectancyAtBirth, by=c("country"))
+data <- merge(data, meanYearsOfSchooling, by=c("country"))
+data <- merge(data, region, by=c("country"))
+data <- merge(data, subregion, by=c("country"))
+#data <- merge(data, averageIQ, by=c("country"))
+#data <- merge(data, violentCrime, by=c("country"))
 
 #write overall data to file
 write.csv(data, "output/data.csv", row.names=FALSE)
@@ -38,8 +55,33 @@ testAndPlotCorrelation(data$grossNationalIncomePerCapita, data$homicideRate,
                        "Gross National Income Per Capita vs Intentional Homicide Rate by Country", 
                        "Gross National Income Per Capita (in US Dollars?)", "Intentional Homicide Rate (per 100 000 people)")
 
+testAndPlotCorrelation(data$HDIRank, data$homicideRate, 
+                       "Human Development Index Rank vs Intentional Homicide Rate by Country", 
+                       "Human Development Index Rank", "Intentional Homicide Rate (per 100 000 people)")
+
+testAndPlotCorrelation(data$HDIValue, data$homicideRate, 
+                       "Human Development Index Value vs Intentional Homicide Rate by Country", 
+                       "Human Development Index Value", "Intentional Homicide Rate (per 100 000 people)")
+
+testAndPlotCorrelation(data$HDIRank, data$homicideRate, 
+                       "Human Development Index Value vs Intentional Homicide Rate by Country", 
+                       "Human Development Index Value", "Intentional Homicide Rate (per 100 000 people)")
+
+testAndPlotCorrelation(data$homicideByFirearmRate, data$homicideRate, 
+                       "Homicide By Firearm Rate vs Intentional Homicide Rate by Country", 
+                       "Homicide By Firearm Rate (per 100 000 people?)", "Intentional Homicide Rate (per 100 000 people)")
+
+testAndPlotCorrelation(data$lifeExpectancyAtBirth, data$homicideRate, 
+                       "Life Expectancy (At Birth) vs Intentional Homicide Rate by Country", 
+                       "Life Expectancy (At Birth)", "Intentional Homicide Rate (per 100 000 people)")
+
+testAndPlotCorrelation(data$meanYearsOfSchooling, data$homicideRate, 
+                       "Mean Years of Schooling vs Intentional Homicide Rate by Country", 
+                       "Mean Years of Schooling", "Intentional Homicide Rate (per 100 000 people)")
+
+#testAndPlotCorrelation(data$averageIQ, data$homicideRate, 
+                      # "Average IQ vs Intentional Homicide Rate by Country", 
+                      # "Average IQ", "Intentional Homicide Rate (per 100 000 people)")
 
 
-#testing output
-#sink("output/testoutput.txt", append=FALSE, split=TRUE)
-#cor.test(data$ownership.rate,data$homicide.rate)
+
